f880852b8efd357e6ebf29b5529c8e8645e91e22
Ashley <schuetta@uwm.edu>
2014-09-28 13:53:39 -0500

    some changes..

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,9 +1,22 @@
 var Controllers = new angular.module('Controllers', []);
 
-Controllers.controller('Main', ['$scope', '$location',
-	function ($scope, $location){
+Controllers.controller('Main', ['$scope', '$location', '$timeout',
+	function ($scope, $location, $timeout){
+		$scope.page = $location['$$url'];
+		
+		$scope.refresh = function()
+		{
+			setTimeout(function () {
+				$scope.$apply(function(){
+					$scope.page = $location['$$url'];
+				});			
+			}, 100);
+			console.log("in here");
+		}
 
-		$scope.page = $location['$$url'] == '/' ;
+		//$scope.page = $location['$$url'] == '/' ;
+		//$scope.$apply;
+
 		console.log($scope.page);
 		
 	}
